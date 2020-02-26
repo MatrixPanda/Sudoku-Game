af26952b5697ac3958b2f0fbbb2b4c84aa821552
MatrixPanda <zehan11s@hotmail.com>
2020-02-26 00:52:31 -0500

    initial commit

---
+++ scripts/sudoku.js
@@ -0,0 +1,39 @@
+window.onload = function() {
+   
+}
+
+
+var boardData = [
+   -1,  1, -1, -1, -1, -1, -1,  9, -1,
+   -1, -1,  4, -1, -1, -1,  2, -1, -1,
+   -1, -1,  8, -1, -1,  5, -1, -1, -1,
+   -1, -1, -1, -1, -1, -1, -1,  3, -1,
+    2, -1, -1, -1,  4, -1,  1, -1, -1,
+   -1, -1, -1, -1, -1, -1, -1, -1, -1,
+   -1, -1,  1,  8, -1, -1,  6, -1, -1,
+   -1,  3, -1, -1, -1, -1, -1,  8, -1,
+   -1, -1,  6, -1, -1, -1, -1, -1, -1
+];
+
+// A convenience function to convert between x and y coordinates in the 2D grid the 1D array
+function boardPosition(x, y) {
+   return y * 9 + x;
+}
+
+function sameBlock(x1, y1, x2, y2) {
+   let firstRow = Math.floor(y1 / 3) * 3;
+   let firstCol = Math.floor(x1 / 3) * 3;
+   return (y2 >= firstRow && y2 <= (firstRow + 2) && x2 >= firstCol && x2 <= (firstCol + 2));
+}
+
+function sameRow(x1, y1, x2, y2) {
+   return y1 == y2;
+}
+
+function sameColumn(x1, y1, x2, y2) {
+   return x1 == x2;
+}
+
+function overlaps(x1, y1, x2, y2) {
+   return sameBlock(x1, y1, x2, y2) || sameRow(x1, y1, x2, y2) || sameColumn(x1, y1, x2, y2);
+}
